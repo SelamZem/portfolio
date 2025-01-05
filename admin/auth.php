c231e5466aae0d255eea4be09af2201ea87815de
SelamZem <110352045+SelamZem@users.noreply.github.com>
2025-01-05 22:00:08 +0300

    Update auth.php

--- admin/auth.php
+++ admin/auth.php
@@ -1,13 +1,11 @@
 <?php
 
-// Start session if not already started
 if (session_status() == PHP_SESSION_NONE) {
     session_start(); 
 }
 
-// Function to check if the admin is authenticated
+
 function checkAuth() {
-    // Check if the session variable 'admin_logged_in' is set to true
     if (!isset($_SESSION['admin_id']) || !isset($_SESSION['admin_username'])) {
         // If not authenticated, redirect to login page
         header("Location: login.php");
@@ -15,7 +13,6 @@ function checkAuth() {
     }
 }
 
-// Function to log in the admin
 function loginAdmin($username, $user_id) {
     // Set session variables for logged-in status, username, and admin ID
     $_SESSION['admin_logged_in'] = true;
@@ -23,7 +20,6 @@ function loginAdmin($username, $user_id) {
     $_SESSION['admin_id'] = $user_id;
 }
 
-// Function to log out the admin
 function logoutAdmin() {
     // Unset all session variables and destroy the session
     session_unset(); 
